d54eb0410d14b580c4b851bb06ef5e931c459a14
farhan <a.farhan79@gmail.com>
2026-01-01 21:40:21 +0100

    feat: added Components and Boards controller to populate boards and components.

---
+++ SMT/API/Controllers/BoardsController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SMT.Application.DTOs;
+using SMT.Application.Models;
+using SMT.Application.Services;
+using System;
+using System.Threading.Tasks;
+
+namespace SMT.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize(AuthenticationSchemes = "Firebase")]
+    public class BoardsController : ControllerBase
+    {
+        private readonly BoardService _boardService;
+        private readonly ILogger<OrdersController> _logger;
+
+
+        public BoardsController(
+         BoardService boardService,
+         ILogger<OrdersController> logger
+         )
+        {
+            _boardService = boardService;
+            _logger = logger;
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var boards = await _boardService.GetAllBoardsAsync();
+
+            if (boards == null)
+            {
+                return NotFound("No boards found!");
+            }
+            // Map entities to DTOs
+            var result = boards.Select(b => new BoardDto
+            {
+                Id = b.Id,
+                Name = b.Name,
+                Description = b.Description,
+                Length = b.Length,
+                Width = b.Width,
+            }).ToList();
+
+            return Ok(result);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] BoardDto boardDto)
+        {
+            _logger.LogInformation("Create order request started for Order {BoardName}", boardDto.Name);
+            var board = await _boardService.CreateBoardAsync(
+
+                boardDto.Name,
+                boardDto.Description,
+                boardDto.Length,
+                boardDto.Width
+            );
+
+            if (board== null)
+            {
+                _logger.LogError("Board request failed for Board {OrderName}", boardDto.Name);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+            // Map to DTO
+            var createdBoard = new BoardDto
+            {
+                Id = board.Id,
+                Name = board.Name,
+                Description = board.Description,
+                Length = board.Length,
+                Width = board.Width
+            };
+
+            _logger.LogInformation("Create order request completed for Order {BoardName}", board.Name);
+
+            return CreatedAtAction(nameof(GetAll), new { id = board.Id }, boardDto);
+        }
+    }
+}
+
---
+++ SMT/API/Controllers/ComponentsController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SMT.Application.DTOs;
+using SMT.Application.Models;
+using SMT.Application.Services;
+using System;
+using System.Threading.Tasks;
+
+namespace SMT.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize(AuthenticationSchemes = "Firebase")]
+    public class ComponentsController : ControllerBase
+    {
+        private readonly ComponentService _componentService;
+        private readonly ILogger<OrdersController> _logger;
+
+
+        public ComponentsController(
+         ComponentService componentService,
+         ILogger<OrdersController> logger
+         )
+        {
+            _componentService = componentService;
+            _logger = logger;
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var components = await _componentService.GetAllComponentsAsync();
+
+            if (components == null)
+            {
+                return NotFound("No component found!");
+            }
+            // Map entities to DTOs
+            var result = components.Select(c => new ComponentDto
+            {
+               Id = c.Id,
+               Name = c.Name,
+               Description = c.Description
+            }).ToList();
+
+            return Ok(result);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] ComponentDto componentDto)
+        {
+            _logger.LogInformation("Create order request started for Order {BoardName}", componentDto.Name);
+            var component = await _componentService.CreateComponentAsync(
+
+                componentDto.Name,
+                componentDto.Description
+            );
+
+            if (component == null)
+            {
+                _logger.LogError("Board request failed for Board {OrderName}", componentDto.Name);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+            // Map to DTO
+            var createdComponent = new BoardDto
+            {
+                Id = component.Id,
+                Name = component.Name,
+                Description = component.Description
+            };
+
+            _logger.LogInformation("Create component request completed for Order {ComponentName}", createdComponent.Name);
+
+            return CreatedAtAction(nameof(GetAll), new { id = component.Id }, componentDto);
+        }
+    }
+}
+
